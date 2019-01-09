5eacced04319a8a08972f2e2052bffd531d09d1a
brooke perry and Sheila Stephen <brooke@>
2019-01-08 16:34:30 -0800

    work on functionality

--- spec/sudoku-spec.js
+++ spec/sudoku-spec.js
@@ -4,9 +4,22 @@ describe('Sudoku', function() {
 
   it('should make an array of 9 numbers', function() {
       var sudoku = new Sudoku(3,4,5,7,8,6,1,2,9);
-      var sudokuArray = sudoku.makeArray();
-      expect(sudoku.makeArray()).toEqual([3,4,5,7,8,6,1,2,9]);
+      var sudokuArray = sudoku.makeRowArray();
+      expect(sudoku.makeRowArray()).toEqual([3,4,5,7,8,6,1,2,9]);
       expect(sudokuArray.length).toEqual(9);
+    });
+
+    it('should check that numbers in the array are 1-9', function() {
+        var sudoku = new Sudoku(3,4,5,7,8,6,1,2,9);
+        var sudokuArray = sudoku.makeRowArray();
+        expect(sudokuArray.sort()).toEqual([1,2,3,4,5,6,7,8,9]);
+    });
+
+    it('should get first row', function() {
+        var sudoku = new Sudoku(3,4,5,7,8,6,1,2,9);
+        var sudokuArray = (sudoku.getRow(1));
+        console.log(sudokuArray);
+        expect(sudokuArray.sort()).toEqual([1,2,3,4,5,6,7,8,9]);
     });
 
 });
--- src/sudoku.js
+++ src/sudoku.js
@@ -1,4 +1,12 @@
-export function Sudoku(num1, num2, num3, num4, num5, num6, num7, num8, num9) {
+export function Sudoku(num1, num2, num3, num4, num5, num6, num7, num8, num9,
+                       num10, num11, num12, num13, num14, num15, num16, num17, num18,
+                       num19, num20, num21, num22, num23, num24, num25, num26, num27,
+                       num28, num29, num30, num31, num32, num33, num34, num35, num36,
+                       num37, num38, num39, num40, num41, num42, num43, num44, num45,
+                       num46, num47, num48, num49, num50, num51, num52, num53, num54,
+                       num55, num56, num57, num58, num59, num60, num61, num62, num63,
+                       num64, num65, num66, num67, num68, num69, num70, num71, num72,
+                       num73, num74, num75, num76, num77, num78, num79, num80, num81) {
   this.num1 = num1;
   this.num2 = num2;
   this.num3 = num3;
@@ -8,9 +16,120 @@ export function Sudoku(num1, num2, num3, num4, num5, num6, num7, num8, num9) {
   this.num7 = num7;
   this.num8 = num8;
   this.num9 = num9;
+
+  this.num10 = num10;
+  this.num11 = num11;
+  this.num12 = num12;
+  this.num13 = num13;
+  this.num14 = num14;
+  this.num15 = num15;
+  this.num16 = num16;
+  this.num17 = num17;
+  this.num18 = num18;
+
+  this.num19 = num19;
+  this.num20 = num20;
+  this.num21 = num21;
+  this.num22 = num22;
+  this.num23 = num23;
+  this.num24 = num24;
+  this.num25 = num25;
+  this.num26 = num26;
+  this.num27 = num27;
+
+  this.num28 = num28;
+  this.num29 = num29;
+  this.num30 = num30;
+  this.num31 = num31;
+  this.num32 = num32;
+  this.num33 = num33;
+  this.num34 = num34;
+  this.num35 = num35;
+  this.num36 = num36;
+
+  this.num37 = num37;
+  this.num38 = num38;
+  this.num39 = num39;
+  this.num40 = num40;
+  this.num41 = num41;
+  this.num42 = num42;
+  this.num43 = num43;
+  this.num44 = num44;
+  this.num45 = num45;
+
+  this.num46 = num46;
+  this.num47 = num47;
+  this.num48 = num48;
+  this.num49 = num49;
+  this.num50 = num50;
+  this.num51 = num51;
+  this.num52 = num52;
+  this.num53 = num53;
+  this.num54 = num54;
+
+  this.num55 = num55;
+  this.num56 = num56;
+  this.num57 = num57;
+  this.num58 = num58;
+  this.num59 = num59;
+  this.num60 = num60;
+  this.num61 = num61;
+  this.num62 = num63;
+  this.num63 = num63;
+
+  this.num64 = num64;
+  this.num65 = num65;
+  this.num66 = num66;
+  this.num67 = num67;
+  this.num68 = num68;
+  this.num69 = num69;
+  this.num10 = num10;
+  this.num71 = num71;
+  this.num72 = num72;
+
+  this.num73 = num73;
+  this.num74 = num74;
+  this.num75 = num75;
+  this.num76 = num76;
+  this.num77 = num77;
+  this.num78 = num78;
+  this.num79 = num79;
+  this.num80 = num80;
+  this.num81 = num81;
 }
 
-Sudoku.prototype.makeArray = function() {
-  var newArray = [this.num1, this.num2, this.num3, this.num4, this.num5, this.num6, this.num7, this.num8, this.num9];
-  return newArray;
+Sudoku.prototype.getRow = function(rowNumber) {
+  var rowArray = []
+  var endIndex =  (rowNumber * 9)-1;
+  var startIndex
+
+    if (endIndex >= 8) {
+      startIndex = indexOf[endIndex - 8];
+    } else startIndex = 1;
+
+  for (var i = startIndex; i <= endIndex; i++) {
+    rowArray.push(i);
+  }
+  return rowArray;
+}
+
+Sudoku.prototype.makeRowArray = function() {
+  var newRowArray1 = [this.num1, this.num2, this.num3, this.num4, this.num5, this.num6, this.num7, this.num8, this.num9];
+
+  var newRowArray2 = [this.num10, this.num11, this.num12, this.num13, this.num14, this.num15, this.num16, this.num17, this.num18];
+
+  var newRowArray3 = [this.num19, this.num20, this.num21, this.num22, this.num23, this.num24, this.num25, this.num26, this.num27];
+
+  var newRowArray4 = [this.num28, this.num29, this.num30, this.num31, this.num32, this.num33, this.num34, this.num35, this.num36];
+
+  var newRowArray5 = [this.num37, this.num38, this.num39, this.num40, this.num41, this.num42, this.num43, this.num44, this.num45];
+
+  var newRowArray6 = [this.num46, this.num47, this.num48, this.num49, this.num50, this.num51, this.num52, this.num53, this.num54];
+
+  var newRowArray7 = [this.num55, this.num56, this.num57, this.num58, this.num59, this.num60, this.num61, this.num62, this.num63];
+
+  var newRowArray8 = [this.num64, this.num65, this.num66, this.num67, this.num68, this.num69, this.num70, this.num71, this.num72];
+
+  var newRowArray9 = [this.num73, this.num74, this.num75, this.num76, this.num77, this.num78, this.num79, this.num80, this.num81];
+  // return newArray;
 };
